64425627d652c82a794ced185e86ef2556f22058
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-09-29 22:35:53 +0530

    Set up routing and components in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import Store from "./components/Pages/Store";
+import Navbar from "./components/Header/Navbar";
+import Contact from "./components/Pages/Contact";
+import ProductDetail from "./components/Pages/ProductDetail";
+import Cart from "./components/Pages/Cart";
+import ErrorPage from "./components/Pages/ErrorPage";
+
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/productDetail/id:" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
+  )
 }
 
 export default App;
