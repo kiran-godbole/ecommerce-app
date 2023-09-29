bd46bfbfe629ba21db52e6e388719ae167313458
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-09-29 22:33:56 +0530

    Add Home component with basic structure

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Slider from './Slider'
+import ProductDetail from './ProductDetail'
+
+const Home = () => {
+  return (
+    <>          
+                
+                    <div className='card-body'>
+                        <Slider/>
+                        <ProductDetail/>
+                    </div>
+               
+            
+        </>
+  )
+}
+
+export default Home
+
