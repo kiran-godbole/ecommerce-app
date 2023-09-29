5f0a42a7060731cbb6428641928a5e749bf129a8
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-09-29 22:33:30 +0530

    Add Store component with basic structure

---
+++ src/components/Pages/Store.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Store = () => {
+  return (
+    <>
+            <div className='container'>
+                <div className='card'>
+                    <div className='card-body'>
+                        <h2>Store</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+  )
+}
+
+export default Store
