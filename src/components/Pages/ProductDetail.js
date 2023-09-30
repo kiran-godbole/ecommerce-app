af27e63a9b85a170910ce19bc4a93bf94174a8bc
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-09-30 10:57:17 +0530

    Add product listing to ProductDetail component

--- src/components/Pages/ProductDetail.js
+++ src/components/Pages/ProductDetail.js
@@ -4,8 +4,43 @@ import imgTwo from '../../Images/products/imgTwo.jpg';
 import imgThree from '../../Images/products/imgThree.jpg';
 import imgFour from '../../Images/products/imgFour.jpg';
 import imgFive from '../../Images/products/imgFive.jpg'
+
 import '../Pages/Produt.css';
-import Footer from '../Footer/Footer';
+
+
+const products = [
+  {
+      title: 'Colors ',
+      price: 100,
+      imageUrl: imgFive,
+  },
+  {
+      title: 'Pink Painting ',
+      price: 50,
+      imageUrl: imgTwo,
+  },
+  {
+      title: 'Black Ring',
+      price: 70,
+      imageUrl: imgThree,
+  },
+  {
+      title: 'Pink Dressed Girl',
+      price: 100,
+      imageUrl: imgFour,
+  },
+  {
+    title: 'Black Colors',
+    price: 70,
+    imageUrl: imageOne,
+},
+{
+  title: ' Colors',
+  price: 70,
+  imageUrl: imgFive,
+},
+];
+
 
 const ProductDetail = () => {
 
@@ -15,69 +50,19 @@ const ProductDetail = () => {
         <div class="container ">
           <h1 class="fs-1  text-center text-capitalize">Music</h1>
           <div className="row gy-3">
-            <div className="col-4">
-              <div class="card" >
-                <img src={imageOne} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h1 class="card-title">Album 1</h1>
-                  <h4 class="card-text  "> $96.85</h4>
-                  <button type="submit" class="btn btn-outline-warning px-4 py-2">ADD TO CART</button>
-                </div>
-              </div>
-
-            </div>
-            <div className="col-4">
-              <div class="card" >
-                <img src={imgTwo} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h1 class="card-title">Album 2</h1>
-                  <h4 class="card-text"> $96.85</h4>
-                  <button type="submit" class="btn btn-outline-warning px-4 py-2">ADD TO CART</button>
-                </div>
-              </div>
-
-            </div>
-            <div className="col-4">
-              <div class="card" >
-                <img src={imgFour} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h1 class="card-title">Album 3</h1>
-                  <h4 class="card-text"> $96.85</h4>
-                  <button type="submit" class="btn btn-outline-warning px-4 py-2">ADD TO CART</button>
-                </div>
-              </div>
-
-            </div>
-            <div className="col-4">
+          {products.map((product, index) => (
+            <div className="col-4 ">
               <div class="card " >
-                <img src={imgTwo} class="card-img-top" alt="..." />
-                <div class="card-body ">
-                  <h1 class="card-title">Album 4</h1>
-                  <h4 class="card-text "> $96.85</h4>
+                <img src={product.imageUrl} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h1 class="card-title">{product.title}</h1>
+                  <h4 class="card-text  ">Rs. {product.price}</h4>
                   <button type="submit" class="btn btn-outline-warning px-4 py-2 ">ADD TO CART</button>
                 </div>
               </div>
             </div>
-            <div className="col-4">
-              <div class="card" >
-                <img src={imgFive} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h1 class="card-title">Album 5</h1>
-                  <h4 class="card-text"> $96.85</h4>
-                  <button type="submit" class="btn btn-outline-warning px-4 py-2">ADD TO CART</button>
-                </div>
-              </div>
-            </div>
-            <div className="col-4">
-              <div class="card" >
-                <img src={imgThree} class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h1 class="card-title">Album 6</h1>
-                  <h4 class="card-text"> $96.85</h4>
-                  <button type="submit" class="btn btn-outline-warning px-4 py-2">ADD TO CART</button>
-                </div>
-              </div>
-            </div>
+             ))}
+          
           </div>
         </div>
         <div class="container my-md-3">
