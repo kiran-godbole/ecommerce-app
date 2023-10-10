6aa0c819a1b7f4ef13aa52597adb72c423fad8fb
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-10-10 21:00:14 +0530

    cart component created with almost al  feature

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,52 @@
+// Cart.js
+import React from "react";
+import { useCart } from './CartContext';
+import './Cart.css';
+import { FiShoppingCart } from "react-icons/fi";
+
+const Cart = () => {
+  const { cartCount, cartItems, addToCart, removeFromCart } = useCart();
+
+  const handleRemoveFromCart = (product) => {
+    removeFromCart(product);
+  };
+
+  // Calculate the total cost of all items in the cart
+  const totalCost = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  return (
+    <>
+      <div className="btn dropstart cart-button">
+        <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          <FiShoppingCart className='cart-trolley' />
+          <span className='cart-total'>
+            {cartCount}
+          </span>
+        </button>
+        <div className="dropdown-menu cart-menu">
+          {cartItems.length === 0 ? (
+            <p>Your cart is empty.</p>
+          ) : (
+            <ul className="list-unstyled">
+              {cartItems.map((item, index) => (
+                <li key={index}>
+                  <img src={item.imageUrl} alt={item.title} className="cart-item-image" /> 
+                  <div>{item.title}</div>
+                  <div>Quantity: {item.quantity}</div>
+                  <div>Price: Rs. {item.price}</div>
+                  <button onClick={() => handleRemoveFromCart(item)}>Remove</button>
+                </li>
+              ))}
+            </ul>
+            
+          )}
+          <div className="cart-total">
+                Total: Rs. {totalCost}
+              </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Cart;
