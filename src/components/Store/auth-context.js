fe845373df804c17a15dddd5f21efe6e87484ffa
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-10-31 21:18:59 +0530

    added Auto logout featured

--- src/components/Store/auth-context.js
+++ src/components/Store/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const AuthContext = React.createContext({
     token: '',
@@ -10,18 +10,37 @@ const AuthContext = React.createContext({
 export const AuthContextProvider = (props) => {
     const initialToken = localStorage.getItem('token');
     const [token, setToken] = useState(initialToken);
+    const [tokenExpireTime, setTokenExpireTime] = useState(null);
 
     const userIsLoggedIn = !!token;
 
     const loginHandler = (token) => {
         setToken(token)
         localStorage.setItem('token', token);
+        const now = new Date();
+        const fiveMinutesFromNow = new Date(now.getTime() + 5 * 60 * 1000); // 5 minutes from now
+        setTokenExpireTime(fiveMinutesFromNow);
     };
 
     const logoutHandler = () => {
         setToken(null);
+        setTokenExpireTime(null);
         localStorage.removeItem('token');
     };
+
+    useEffect(() => {
+        const checkTokenExpiration = () => {
+            if (tokenExpireTime && new Date() >= tokenExpireTime) {
+                logoutHandler();
+            }
+        };
+
+        const tokenExpirationTimer = setInterval(checkTokenExpiration, 1000); // Check every second
+
+        return () => {
+            clearInterval(tokenExpirationTimer);
+        };
+    }, [tokenExpireTime]);
 
     const contextValue = {
         token: token,
