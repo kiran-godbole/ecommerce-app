e9205943367701a4506fc676a06f6499b0dd195f
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-11-15 17:36:26 +0530

    CartProvider added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { CartProvider } from './components/Cart/CartContext';
+
+import { AuthContextProvider } from './components/Store/auth-context';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <AuthContextProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+ </AuthContextProvider>
+  
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
